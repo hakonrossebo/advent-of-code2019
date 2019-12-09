f1920b34edbd12ed36e18a8e189f86a639cf66fe
Håkon Rossebø <hakonrossebo@hotmail.com>
2019-12-09 18:21:11 +0100

    Sum krampus fiks

--- knowit/day9/day9.fsx
+++ knowit/day9/day9.fsx
@@ -1,47 +1,46 @@
 open System.IO
+let filSti = Path.Combine([| __SOURCE_DIRECTORY__; "krampus.txt" |])
 
-let path = Path.Combine([| __SOURCE_DIRECTORY__; "krampus.txt" |])
-
-let lines() =
-    File.ReadAllLines(path)
+let linjer() =
+    File.ReadAllLines(filSti)
     |> Array.map int64
     |> Array.sort
     |> Array.map string
 
-let splitStringAt (input: string) index =
+let splittStreng (input: string) index =
     if index < 1 || index > input.Length - 1 then
         None
     else
         let part1 = input.[0..index - 1]
-
-        let combination =
-            input.[index..]
+        let part2 = input.[index..]
+        part2
             |> (fun part2 ->
-            if part2 = "0" then None
-            else Some part2)
+            if int64 part2 = 0L then 
+                None
+            else 
+                Some part2)
             |> Option.map (fun part2 -> (part1, part2))
-        combination
 
-let getStringCombinations (input: string) =
+let finnStrengKombinasjoner (input: string) =
     let len = input.Length
-    [ 1 .. len ] |> List.choose (splitStringAt input)
+    [ 1 .. len ] |> List.choose (splittStreng input)
 
-let sumCombinations ((p1, p2): string * string) =
+let summerKombinasjoner ((p1, p2): string * string) =
     let p1Num = int p1
     let p2Num = int p2
     p1Num + p2Num
 
-let verifyKrampus (input: string) =
+let sjekkKrampus (input: string) =
     let inputNumber = int64 input
     string (inputNumber * inputNumber)
-    |> getStringCombinations
-    |> List.map sumCombinations
+    |> finnStrengKombinasjoner
+    |> List.map summerKombinasjoner
     |> List.exists (fun x -> int64 x = inputNumber)
 
-
 let sum =
-    lines()
-    |> Array.map (fun s -> (s, verifyKrampus s))
+    linjer()
+    |> Array.map (fun s -> (int64 s, sjekkKrampus s))
     |> Array.filter (fun (_, isKrampus) -> isKrampus)
-    |> Array.map (fun (s, _) -> int64 s)
-    |> Array.sum
+    |> Array.sumBy (fun (d, _) -> d)
+
+//fasit = 445372L
